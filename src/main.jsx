17a7c518398f805eacf378014c10ff43f1083db4
Andrea-vicari <andrea.vicari77@gmail.com>
2024-03-09 14:44:05 +0100

    added the cookie consent bar in the main.jsx component

--- src/main.jsx
+++ src/main.jsx
@@ -35,14 +35,14 @@ ReactDOM.createRoot(document.getElementById('footer')).render(
         <Footer />
         <CookieConsent
         location="bottom"
-        buttonText="Sure man!!"
+        buttonText="Accept"
         cookieName="myAwesomeCookieName2"
         style={{ background: "#2B373B" }}
         buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
         expires={150}
         >
         This website uses cookies to enhance the user experience.{" "}
-        <span style={{ fontSize: "10px" }}>This bit of text is smaller :O</span>
+        <span style={{ fontSize: "10px" }}>Only functional cookies are used, no adv cookies or any usr data will be collected </span>
         </CookieConsent>
     </div>
 )
