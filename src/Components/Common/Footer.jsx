3e4d20360a1390b255967306fbbd84a79d5d7dc6
Andrea-vicari <andrea.vicari77@gmail.com>
2024-04-22 10:39:57 +0200

    Footer

--- src/Components/Common/Footer.jsx
+++ src/Components/Common/Footer.jsx
@@ -35,7 +35,7 @@ function Footer() {
             <hr></hr>
             <p className='text-black'>&copy;2024 Andrea Vicari || Site developed with <a  href='https://omsel-portfolio.vercel.app/' target='_blank'><span className='text-white'> Omsel </span></a>  React theme</p>
         </div>
-        <button id="myBtn" onClick={scrollToTop} style={{display: visible ? 'inline' : 'none'}} className='btn btn-sm btn-outline-secondary'>
+        <button id="myBtn" onClick={scrollToTop} style={{display: visible ? 'inline' : 'none'}} className='btn btn-sm btn-warning'>
             <i className="bi bi-arrow-bar-up"></i>
           </button>
     </footer>
